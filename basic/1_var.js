b4d7b5821b252964a302ec57df6fcabaf316ce7c
Tanushka Singh <151910045+tanushkasingh3000@users.noreply.github.com>
2024-06-30 16:59:17 +0000

    Variables and constants

---
+++ basic/1_var.js
@@ -0,0 +1,14 @@
+const accId=1234
+var accName="Tanushka"//preferred not to be used
+let email="tanushkasingh@gmail.com"
+phone=333
+
+console.table([accId,accName,email,phone])
+
+// accId=1234
+accName="Astha"
+email="astha@gmail.com"
+phone=555
+
+
+console.table([accId,accName,email,phone])
